5135ba01c5857a1ba9d1c839bfb0ea67d6b67fd9
Tobias Liebmann <tobias.liebmann@gmx.de>
2017-09-28 19:37:34 +0200

    The incoming data is now better rpesented

--- recieve_data.py
+++ recieve_data.py
@@ -1,5 +1,4 @@
-#Import all the necessary packages
-#This programm is meant to run on Python3.
+#!/home/tobias/anaconda3/bin/python3.6
 
 from arduino_python_communication_v2 import *
 from datetime import datetime
@@ -8,11 +7,13 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_temperature import BrickletTemperature
 
 #Set the parameters for the PID controller.
+#Be carefull when setting a high Kd value and a small sampletime, this will result in the controller outputing either the max. output or the min. output
+#when there are changes in the input.
 fixedPoint=16
-sampleTime=10
+sampleTime=1000
 kp=0.94
 ki=0.07
-kd=187.69
+kd=180.00
 setPoint=30.000
 
 #Needed for the connection to the temperature bricklet.
@@ -21,7 +22,7 @@ PORT=4223
 UID="zFZ" #UID for the currently used sensor.
 #UID="zih" #UID for the other sensor
 
-s = serial.Serial('/dev/ttyUSB1',115200,timeout=0.5)
+s = serial.Serial('/dev/ttyUSB0',115200,timeout=sampleTime)
 #check if the serial port is open
 if not s.isOpen:
         #if it is not open, open it
@@ -30,7 +31,7 @@ if not s.isOpen:
 time.sleep(2) #for Arduino Nano/Uno wait 2 s for Due 1 s is okay.
 
 #Send the main settings to the controller.
-sendIntData({0:22.000,1:kp,2:ki,3:kd,6:1,7:sampleTime,8:0,9:setPoint},s,fixedPoint)
+sendIntData({0:22.000,1:kp,2:ki,3:kd,4:0.0,5:255.0,6:1,7:sampleTime,8:0,9:setPoint},s,fixedPoint)
 
 if __name__ == "__main__":
 
@@ -56,18 +57,35 @@ if __name__ == "__main__":
                 #Read the answer of the Arduino with Cbor and Cobs.
                 if s.in_waiting:
                         while s.in_waiting:
-                                #print(s.in_waiting)
+                                
                                 recievedByte=s.read()
-                                #print(recievedByte)
+                                
                                 if recievedByte==b'\x00':
-                                        #print(data)
-                                        print(cbor2.loads(cobs.decode(data)))
+                                        
+                                        obj=cbor2.loads(cobs.decode(data))
+
+                                        #This part is needed if the number 0.0 is send.
+                                        #0.0 will not be recognized as a 32 bit floating point number but as a SimpleValue.
+                                        #This is the case because simple values and floating point numbers have the same major type in CBOR.
+                                        if obj==cbor2.CBORSimpleValue(0):
+                                                print(0.00, end='', flush=True)
+                                        
+                                        #Floats will be rounded to two numbers after the comma.
+                                        elif type(obj) is float:
+                                                print("%.2f" % obj, end='', flush=True)
+                                        
+                                        #All the other datatypes are printed normally.
+                                        else:
+                                                print(obj, end='', flush=True)
+                                        
+                                        #Reinitialize the bytearray for new data.
                                         data=b''
                                 else:
                                         data = data + recievedByte
 
-                fileToWrite.write(str(temp)+"\n")
                 #The sample is in the unit ms, but the sleep method takes seconds as input so we have to divide by 1000 additionally if we want to wait half the sampletime we get 500.
                 time.sleep(sampleTime/1000) 
 
         ipcon.disconnect()
+
+fileToWrite.close()
